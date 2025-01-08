a76fbc79a3844a75974705605f6f9818168afb76
Alan Varghese <alanvz757@gmail.com>
2025-01-08 13:12:47 +0530

    updated

--- comments.dart
+++ comments.dart
@@ -1,11 +1,11 @@
 main() {
   print("Dart Programming");
 }
-//Learning Dart programming is so interesting
+//Learning Dart programming is so interesting (Dart Singleline Comment)
 
-/*Learning Dart programming is so interesting
+/*Learning Dart programming is so interesting (Dart Multiline Comment)
   
 */
 
-///Learning Dart programming is so interesting
+///Learning Dart programming is so interesting (Dart Documentation Comment)
 ///Learning Dart programming is so interesting
