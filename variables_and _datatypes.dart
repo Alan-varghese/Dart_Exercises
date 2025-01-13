88a999c0df84cffd40cee466ed2b432779b6df6a
Alan Varghese <alanvz757@gmail.com>
2025-01-13 09:18:54 +0530

    Updated

---
+++ variables_and _datatypes.dart
@@ -0,0 +1,7 @@
+void main() {
+  int age = 22;
+  String Name = 'Alan';
+  double Height = 889;
+  bool isDeveloper = true;
+  print('Age=$age\nName=$Name\nHeight=$Height\nDeveloper=$isDeveloper');
+}
